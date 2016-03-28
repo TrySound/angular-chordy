08dbe9ccfdc06ec6b92bd2be21705eebfb536d06
Bogdan Chadkin <trysound@yandex.ru>
2016-03-28 22:45:47 +0300

    Use jasmine spy for fake set controller

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var Server = require('karma').Server;
 
-gulp.task('build', ['test'], function () {
+gulp.task('build', function () {
 	return gulp.src(['src/*.mdl.js', 'src/*.js'])
 		.pipe(concat('angular-chordy.js'))
 		.pipe(babel({
--- test/chordy-chord.spec.js
+++ test/chordy-chord.spec.js
@@ -7,22 +7,24 @@ describe('chordy-chord controller', () => {
 
 	beforeEach(inject(($rootScope, $componentController) => {
 		const scope = $rootScope.$new();
-		function fakeMethod(method) {
-			return function () {
-				this.lastCalled = method;
-			};
-		}
 		fakeSet = {
-			lastCalled: null,
-			add: fakeMethod('add'),
-			remove: fakeMethod('remove'),
-			open: fakeMethod('open'),
-			close: fakeMethod('close'),
-			toggle: fakeMethod('toggle'),
-			pin: fakeMethod('pin'),
-			unpin: fakeMethod('unpin'),
-			togglePin: fakeMethod('togglePin')
+			add: angular.noop,
+			remove: angular.noop,
+			open: angular.noop,
+			close: angular.noop,
+			toggle: angular.noop,
+			pin: angular.noop,
+			unpin: angular.noop,
+			togglePin: angular.noop
 		};
+		spyOn(fakeSet, 'add');
+		spyOn(fakeSet, 'remove');
+		spyOn(fakeSet, 'open');
+		spyOn(fakeSet, 'close');
+		spyOn(fakeSet, 'toggle');
+		spyOn(fakeSet, 'pin');
+		spyOn(fakeSet, 'unpin');
+		spyOn(fakeSet, 'togglePin');
 		component = $componentController('chordyChord', {
 			$scope: scope,
 			$element: angular.element('<div></div>')
@@ -31,49 +33,70 @@ describe('chordy-chord controller', () => {
 		});
 	}));
 
-	it('should init and destroy', () => {
+	it('should init chord', () => {
 		component.$onInit();
-		expect(fakeSet.lastCalled).toBe('add');
+		expect(fakeSet.add).toHaveBeenCalledTimes(1);
+		expect(fakeSet.add).toHaveBeenCalledWith(component);
+	});
+
+	it('should destroy chord', () => {
 		component.$onDestroy();
-		expect(fakeSet.lastCalled).toBe('remove');
+		expect(fakeSet.remove).toHaveBeenCalledTimes(1);
+		expect(fakeSet.remove).toHaveBeenCalledWith(component);
 	});
 
-	it('should have toggle method', () => {
+	it('should toggle chord', () => {
 		component.toggle();
-		expect(fakeSet.lastCalled).toBe('toggle');
-		component.toggle(null, true);
-		expect(fakeSet.lastCalled).toBe('open');
-		component.toggle(null, false);
-		expect(fakeSet.lastCalled).toBe('close');
+		expect(fakeSet.toggle).toHaveBeenCalledTimes(1);
+		expect(fakeSet.toggle).toHaveBeenCalledWith(component);
 	});
 
-	it('should have pin method', () => {
+	it('should open chord', () => {
+		component.toggle(null, true);
+		expect(fakeSet.open).toHaveBeenCalledTimes(1);
+		expect(fakeSet.open).toHaveBeenCalledWith(component);
+	});
+
+	it('should close chord', () => {
+		component.toggle(null, false);
+		expect(fakeSet.close).toHaveBeenCalledTimes(1);
+		expect(fakeSet.close).toHaveBeenCalledWith(component);
+	});
+
+	it('should toggle pin chord', () => {
 		component.pin();
-		expect(fakeSet.lastCalled).toBe('togglePin');
+		expect(fakeSet.togglePin).toHaveBeenCalledTimes(1);
+		expect(fakeSet.togglePin).toHaveBeenCalledWith(component);
+	});
+
+	it('should pin chord', () => {
 		component.pin(null, true);
-		expect(fakeSet.lastCalled).toBe('pin');
+		expect(fakeSet.pin).toHaveBeenCalledTimes(1);
+		expect(fakeSet.pin).toHaveBeenCalledWith(component);
+	});
+
+	it('should unpin chord', () => {
 		component.pin(null, false);
-		expect(fakeSet.lastCalled).toBe('unpin');
+		expect(fakeSet.unpin).toHaveBeenCalledTimes(1);
+		expect(fakeSet.unpin).toHaveBeenCalledWith(component);
 	});
 
 	it('should be able to pass $event as first argument to stop propagation', () => {
-		let calls = 0;
 		const $event = {
-			stopPropagation: () => {
-				calls += 1;
-			}
+			stopPropagation: angular.noop
 		};
+		spyOn($event, 'stopPropagation');
 		component.toggle($event);
-		expect(calls).toBe(1);
+		expect($event.stopPropagation).toHaveBeenCalledTimes(1);
 		component.toggle($event, true);
-		expect(calls).toBe(2);
+		expect($event.stopPropagation).toHaveBeenCalledTimes(2);
 		component.toggle($event, false);
-		expect(calls).toBe(3);
+		expect($event.stopPropagation).toHaveBeenCalledTimes(3);
 		component.pin($event);
-		expect(calls).toBe(4);
+		expect($event.stopPropagation).toHaveBeenCalledTimes(4);
 		component.pin($event, true);
-		expect(calls).toBe(5);
+		expect($event.stopPropagation).toHaveBeenCalledTimes(5);
 		component.pin($event, false);
-		expect(calls).toBe(6);
+		expect($event.stopPropagation).toHaveBeenCalledTimes(6);
 	});
 });
